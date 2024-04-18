c3c21bfe8b8884873c536d7d9707d543be257346
Jérémy PLANCKEEL <jeremy.planckeel@outlook.com>
2024-04-18 15:08:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# ggithub-pr-slack
+
+Chrome plugin to copy in clipboard PR review message for slack.
+
+Compatbile with chrome, chromium like and firefox,
+
+## How to use 
+
+On merge request page, click on buton "Review" for message to clipboard
+
+![example](/images/example.png?raw=true)
+
+Review message is formated 
+
+```
+:please_review: `folder/project` title of merger request [PR123](https://merge-request-url.example)
+```
+Merged message is formated 
+
+```
+:merged: `folder/project` title of merger request [PR123](https://merge-request-url.example)
+```
+
+## Slack 
+
+By default slack don't format markdown text, you have to solution to format markdown on slack. 
+
+- Active `Format messages with markup` in preference/advanced
+
+- Use shortcut to apply markdown format : `Maj + CMD + F`
+
+
+## Build for firefox
+
+Requierments:
+NodeJs v20.10.0 < 21
+Web-ext >7.5.0
+
+```
+nvm install 20
+nvm use node 20
+npm install web-ext
+web-ext sign --api-key <APIKEY> --api-secret <SECRETKEY>
+web-ext build
+```
---
+++ popup.js
@@ -0,0 +1,40 @@
+console.log('<----- Extension script started running ----->');
+
+document.querySelector('.review').addEventListener('click', function () {
+   
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var current = tabs[0];
+        url = current.url;
+        title = current.title;
+        extractTitle = title.match(/(.*) · Pull Request #(.d*) · (.*)/i)
+        console.log(extractTitle)
+        prTitle= extractTitle[1]
+        prId= extractTitle[2]
+        prProject= extractTitle[3].replace(/\s/g, '')
+
+        reviewMessage = ":please_review: `" + prProject + "` " + prTitle + " [PR" + prId +"](" + url + ")"    
+        console.log(reviewMessage)
+        navigator.clipboard.writeText(reviewMessage);
+    });
+
+});
+
+
+document.querySelector('.merged').addEventListener('click', function () {
+   
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var current = tabs[0];
+        url = current.url;
+        title = current.title;
+        extractTitle = title.match(/(.*) · Pull Request #(.d*) · (.*)/i)
+        console.log(extractTitle)
+        prTitle= extractTitle[1]
+        prId= extractTitle[2]
+        prProject= extractTitle[3].replace(/\s/g, '')
+
+        reviewMessage = ":merged: `" + prProject + "` " + prTitle + " [pr" + prId +"](" + url + ")"    
+        console.log(reviewMessage)
+        navigator.clipboard.writeText(reviewMessage);
+    });
+
+});
